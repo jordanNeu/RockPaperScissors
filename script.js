ef33f944ccb106c884a1bb0b45ec84452dc8e2a0
Jordan Neumann <Jneumann26@gmail.com>
2022-06-05 18:17:44 -0400

    Single Round is functional, planning to fix user input case-sensitivity

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 function computerPlay() {
     let computer = ["Rock","Paper","Scissors"];
-    // 'Math.floor' returns the largest integer less than or equal to a given number
-    let computerMove = computer[Math.floor(Math.random()*computer.length)];
-    console.log(computerMove);
+    // 'Math.floor' returns the largest integer less than or equal to a given number 
+    let move = computer[Math.floor(Math.random()*computer.length)];
+    return move;
 }
 
 function playRound( playerSelection, computerSelection) {
-    playerSelection = prompt("What is your choice? Rock Paper or Scissors?");
-    computeSelection = computerPlay();
+    if(playerSelection === computerSelection) {
+        console.log("Draw! Try Again!");
+    }
     if(playerSelection === "Rock" && computerSelection === "Scissors") {
-        console.log("Rock Wins");
+        console.log("You Win!");
+        if(playerSelection === "Scissors" && computerSelection === "Rock") {
+            console.log("Computer Wins");
+        }
     }
     if(playerSelection === "Scissor" && computerSelection ==="Paper") {
-        console.log("Scissors Wins");
+        console.log("You Win!");
+        if(playerSelection === "Paper" && computerrSelection ==="Scissors") {
+            console.log("Computer Wins");
+        }
     }
     if(playerSelection === "Paper" && computerSelection ==="Rock") {
-        console.log("Paper Wins");
+        console.log("You Win!");
+        if(playerSelection === "Rock" && computerSelection ==="Paper") {
+            console.log("Computer Wins");
+        }
     }
 
 }
 
+let playerMove = prompt("What is your choice? Rock, Paper, or Scissors?") 
+console.log(playerMove);
 
+let computerMove = computerPlay();
+console.log(computerMove);
+
+console.log(playRound(playerMove, computerMove));
+let playerScore = 0;
+let computerScore = 0;
