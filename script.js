a19ae97a9aa621601440e51101753a3194b8fa1b
Jordan Neumann <Jneumann26@gmail.com>
2022-06-05 20:09:52 -0400

    Closer to complete, implement score keeping. For loop needs work

--- script.js
+++ script.js
@@ -1,41 +1,89 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = playerMove();
+let computerSelection = computerPlay();
+
+
 function computerPlay() {
     let computer = ["Rock","Paper","Scissors"];
     // 'Math.floor' returns the largest integer less than or equal to a given number 
+    // This function is meant to return a random choice of Rock, Paper or Scissors
     let move = computer[Math.floor(Math.random()*computer.length)];
     return move;
 }
 
-function playRound( playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
-        console.log("Draw! Try Again!");
+function playerMove() {
+    // Writing a function that accept user input, then pushes it to lower-case in order to make user input
+    // case-insensitive. 
+    let user = prompt("What is your choice? Rock, Paper, or Scissors?");
+    let lowerCase = user.toLowerCase();
+    if(lowerCase === "rock") {
+        return "Rock";
     }
-    if(playerSelection === "Rock" && computerSelection === "Scissors") {
-        console.log("You Win!");
-        if(playerSelection === "Scissors" && computerSelection === "Rock") {
-            console.log("Computer Wins");
-        }
+    else if(lowerCase === "paper") {
+        return "Paper";
     }
-    if(playerSelection === "Scissor" && computerSelection ==="Paper") {
-        console.log("You Win!");
-        if(playerSelection === "Paper" && computerrSelection ==="Scissors") {
-            console.log("Computer Wins");
-        }
+    else if(lowerCase === "scissors") {
+        return "Scissors";
     }
-    if(playerSelection === "Paper" && computerSelection ==="Rock") {
-        console.log("You Win!");
-        if(playerSelection === "Rock" && computerSelection ==="Paper") {
-            console.log("Computer Wins");
-        }
-    }
-
 }
 
-let playerMove = prompt("What is your choice? Rock, Paper, or Scissors?") 
-console.log(playerMove);
+function playRound(playerSelection, computerSelection) {
+    // playRound plays one round of Rock, Paper Scissors with the usual rules
+    // if there is a draw the game the round will start over
+    if(playerSelection === computerSelection) {
+        return "Draw! Try Again!";
+    }
+    else if(playerSelection === "Rock" && computerSelection === "Scissors") {
+        playerScore++;
+        return "You Win!";
+        
+    }
+    else if(playerSelection === "Scissors" && computerSelection === "Rock") {
+        computerScore++;
+        return "Computer Wins :(";
+        
+    }
+    else if(playerSelection === "Scissor" && computerSelection ==="Paper") {
+        playerScore++;
+        return "You Win!";
+        
+    }
+    else if(playerSelection === "Paper" && computerSelection ==="Scissors") {
+        computerScore++;
+        return "Computer Wins :(";
+        
+    }
+    else if(playerSelection === "Paper" && computerSelection ==="Rock") {
+        playerScore++;
+        return "You Win!";
+        
+    }
+    else if(playerSelection === "Rock" && computerSelection ==="Paper") {
+        computerScore++;
+        return "Computer Wins :(";
+        
+    }
+}
 
-let computerMove = computerPlay();
-console.log(computerMove);
+function game () {
+    for (let i = 1; i <= 5; i++) {
+        playerMove();
+        computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
 
-console.log(playRound(playerMove, computerMove));
-let playerScore = 0;
-let computerScore = 0;
+    }
+}
+
+function winGame() {
+    if (playerScore = 5) {
+        alert("You Win the Game!");
+    }
+    else if (computerScore = 5) {
+        alert("You Lost!");
+    }
+}
+
+game();
+console.log(playerScore);
+console.log(computerScore);
